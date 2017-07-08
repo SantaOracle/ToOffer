09e1f3b7faab58e59a09dffc2b1a6e0a2a69056a
SantaOracle <524372977@qq.com>
2017-07-08 22:34:16 +0800

    剑指OFFER第一题，新建Module

---
+++ ToOfferCodes/1-SearchFromBinaryArray/src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Link:https://www.nowcoder.com/practice/abc3fe2ce8e146608e868a70efebf62e?tpId=13&tqId=11154&tPage=1&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
+ * Description: 在一个二维数组中，每一行都按照从左到右递
+ *  增的顺序排序，每一列都按照从上到下递增的顺序排序。请完
+ *  成一个函数，输入这样的一个二维数组和一个整数，判断数组
+ *  中是否含有该整数。
+ * Created by Administrator on 2017/7/8.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ ToOfferCodes/1-SearchFromBinaryArray/src/Solution.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2017/7/8.
+ */
+public class Solution {
+
+    public boolean Find(int target, int [][] array) {
+
+        return false;
+    }
+}
