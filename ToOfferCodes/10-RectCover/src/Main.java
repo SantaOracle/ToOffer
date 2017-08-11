4b0b47503d3acb2a303b0e7cda42db8f6ae4d23a
SantaOracle <524372977@qq.com>
2017-08-11 10:44:08 +0800

    剑指OFFER第十题，完成

---
+++ ToOfferCodes/10-RectCover/src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2017/8/11.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.RectCover(4));
+    }
+}
